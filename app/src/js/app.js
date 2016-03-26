86e204154a8d63884db3dd4d1dbcc340fb19a68b
scojo <info@alisablanter.comm>
2016-03-26 11:37:40 -0400

    converting location into lat/lon, making request to open-notify, formatting timestamp

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -27,13 +27,15 @@
     $scope.searchInit = function() {
 
       $scope.loading = true;
+      //need to turn location into lat/lon
+      var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + $scope.location + '&key=AIzaSyCLH--06Xt3eiKJSL3QOANF0qNZh_KdvtA';
 
-      $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + $scope.location + '&key=AIzaSyCLH--06Xt3eiKJSL3QOANF0qNZh_KdvtA')
+      $http.get(url)
 
       .then(function(response) {
 
         $scope.loading = false;
-
+        //there could be any number of results, (5 cities named Paris for example), we're just going to grab the first one
         var lat = response.data.results[0].geometry.location.lat,
             lon = response.data.results[0].geometry.location.lng;
 
@@ -44,11 +46,13 @@
     }, 
     requestISSPass = function(lat, lon) {
 
-      $http.get('http://api.open-notify.org/iss-pass.json?lat=' + lat + '&lon=' +lon).
+      $http.jsonp('http://api.open-notify.org/iss-pass.json?lat=' + lat + '&lon=' + lon + '&n=1' + '&callback=JSON_CALLBACK').
 
-      then(function(response) {
+      success(function(data) {
 
-        console.log(response)
+       var time = data.response[0].risetime;
+
+        console.log()
 
       });
 
